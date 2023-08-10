f5ad05575e0b06b41c0e26fc1aa86a9864141896
louaymb <84675541+louaymb@users.noreply.github.com>
2023-08-10 15:26:47 +0200

    Create OnboardingViewController.swift

---
+++ OnboardingViewController.swift
@@ -0,0 +1,11 @@
+import UIKit
+
+class OnboardingViewController: UIViewController {
+
+	override func viewDidLoad() {
+    	super.viewDidLoad()
+
+    	view.backgroundColor = .white
+    	navigationItem.title = "Onboarding"
+	}
+}
